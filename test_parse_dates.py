021504ef1160260d58c631d2d749f65f4ea6f4a7
David Mershon <dmershon@universalhappymaker.com>
2020-09-27 11:25:50 -0700

    tried out tuples and named tuples in my test parse dates class

--- test_parse_dates.py
+++ test_parse_dates.py
@@ -1,8 +1,34 @@
 import re
+from collections import namedtuple
+
 
 def main():
-    formatted_dates = [ "before 12th century{ds||1|a|}", "14th century{ds||1||}", "1519{ds||1|a|}", "1526", "1949",
-    "15th century{ds||1||}", "circa 1842", "1st century", "2nd century", "3rd century", "4th century", "dog", "23 cat" ]
+    #test_parse_formatted_dates()
+    #test_tuple()
+    test_named_tuple()
+
+
+def test_named_tuple():
+    DatedWord = namedtuple("DatedWord", "first_use was_parsed")
+    test_success_word = DatedWord(first_use="1926", was_parsed=True)
+    test_fail_word = DatedWord(first_use=None, was_parsed=False)
+    print("test_named_tuple...")
+    print(f"test_success_word first_use: {test_success_word.first_use} | was_parsed: {test_success_word.was_parsed}")
+    print(f"test_fail_word first_use: {test_fail_word.first_use} | was_parsed: {test_fail_word.was_parsed}")
+
+
+def test_tuple():
+    tuple_test = ("dog", 3.724, ("meep", "meep2"), "cat", 257, True, "bear")
+    for word in tuple_test:
+        print(word)
+    
+    is_true = tuple_test[5]
+    print(f"testing tuple indexing - is_true: {is_true}")
+
+
+def test_parse_formatted_dates():
+    formatted_dates = ( "before 12th century{ds||1|a|}", "14th century{ds||1||}", "1519{ds||1|a|}", "1526", "1949",
+    "15th century{ds||1||}", "circa 1842", "1st century", "2nd century", "3rd century", "4th century", "dog", "23 cat" )
 
     print("printing formatted dates...")
     for fd in formatted_dates:
@@ -20,7 +46,6 @@ def main():
                 message += "(before)"
 
         print(message)
-
 
 
 def regex_find(formatted_date, search_pattern):
