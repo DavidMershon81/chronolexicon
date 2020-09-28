d68c2ec30a63739b7faca1251fb0de3cfc50f481
David Mershon <dmershon@universalhappymaker.com>
2020-09-28 08:48:19 -0700

    added a test function to try and separate words from spaces and punctuation

--- test_parse_dates.py
+++ test_parse_dates.py
@@ -4,10 +4,24 @@ from collections import namedtuple
 DatedWord = namedtuple("DatedWord", "first_use was_parsed")
 
 def main():
+    #test_separate_words()
     test_parse_formatted_dates()
-    #test_tuple()
-    #test_named_tuple()
 
+
+def test_separate_words():
+    sample_text = "This is 25 sample pieces of text. Don't knock it!"
+
+    # this was the algoirthm I used to count words in readabilty, but it doesn't quite work for this application
+    # I think i need to modify it to pull a set of characters that would be in a word, followed by all the characters
+    # that follow that word, which are not word characters
+    #words = regex_find_all_matches(sample_text, r"\S+[\s|.|!|?]")
+
+    # I could also try splitting a string based on spaces and punctuation characters
+    #maybe I can get somewhere with the partition routine too
+    words = sample_text.partition(" ")
+    for word in words:
+        print(f"word: [{word}]")
+    
 
 def test_named_tuple():
     test_success_word = DatedWord(first_use="1926", was_parsed=True)
@@ -40,14 +54,14 @@ def test_parse_formatted_dates():
 def parse_formatted_date(formatted_date):
     #print(f"parsing formatted_date:{formatted_date}...")
 
-    four_digit_date = regex_find(formatted_date, r"\d{4}")
-    century_date_raw = regex_find(formatted_date, r"(\d{1,2})(th|st|rd|nd)( century)")
+    four_digit_date = regex_find_one_match(formatted_date, r"\d{4}")
+    century_date_raw = regex_find_one_match(formatted_date, r"(\d{1,2})(th|st|rd|nd)( century)")
 
     if four_digit_date: 
         #print(f"found four_digit_date: {four_digit_date}")
         return DatedWord(first_use=int(four_digit_date), was_parsed=True)
     elif century_date_raw:
-        is_before = regex_find(formatted_date, r"before")
+        is_before = regex_find_one_match(formatted_date, r"before")
         first_use_century = int(century_date_raw[0]) - 1
         if is_before:
             first_use_century -= 1
@@ -58,12 +72,17 @@ def parse_formatted_date(formatted_date):
         return DatedWord(first_use=None, was_parsed=False)
 
 
-def regex_find(formatted_date, search_pattern):
-    pattern = re.compile(search_pattern)
-    matches = pattern.findall(formatted_date)
+def regex_find_one_match(text, search_pattern):
+    matches = regex_find_all_matches(text, search_pattern)
     if len(matches) > 0:
         return matches[0]
     else:
         return None
+
+
+def regex_find_all_matches(text, pattern):
+    pattern = re.compile(pattern)
+    return pattern.findall(text)
+
 
 main()
