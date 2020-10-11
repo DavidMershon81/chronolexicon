616ff965ba2e8032e51cbafdffbd64ce8fb8fcd3
David Mershon <dmershon@universalhappymaker.com>
2020-10-10 17:26:03 -0700

    integrated the database searching from database_test into the main chronolexicon program and removed the test script

--- chronolexicon.py
+++ chronolexicon.py
@@ -1,10 +1,12 @@
 import os
+from datetime import date
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
-from login_required import login_required
-from word_age_info import search_api_for_word_first_use
+from word_age_info import search_api_for_word_first_use, DatedWord
 from word_separator import separate_words_and_punctuation, DatedWordPunctuationPair
+from regex_helper import regex_find_one_match
 
 # Configure application
 app = Flask(__name__)
@@ -24,6 +26,13 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+# Initialize database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
+
+#start session
 Session(app)
 
 # Make sure API key is set
@@ -45,23 +54,56 @@ def analyze_text_first_use():
 
     if analysis_text:
         words_and_punctuation = separate_words_and_punctuation(analysis_text)
-        dated_words = [DatedWordPunctuationPair(word=wp[0], punctuation=wp[1], first_use_info=search_api_for_word_first_use(wp[0])) for wp in words_and_punctuation]
+        dated_words = [DatedWordPunctuationPair(word=wp[0], punctuation=wp[1], first_use_info=find_first_word_use(wp[0])) for wp in words_and_punctuation]
         analyzed_text_html = render_template("analyzed_text.html", dated_words=dated_words)
         return jsonify({"analyzed_text_html" : analyzed_text_html })
 
     return jsonify({"error" : "Missing data!"})
 
 
-@app.route("/test_login_required")
-@login_required
-def test_login_required():
-    """Test login required page"""
-    return "Test rendering index page"
+#Word parsing and database management functions
+def find_first_word_use(word_raw):
+    word = word_raw.lower()
+
+    number_match = regex_find_one_match(word, r"\d+")
+
+    if number_match:
+        return DatedWord(word_lower=word, first_use=None, was_parsed=False)
+    else:
+        db_match = find_word_in_db(word)
+
+        if db_match:
+            return DatedWord(word_lower=db_match.word, first_use=db_match.first_use_date, was_parsed=db_match.first_use_known)
+        else:
+            queries_today = find_api_queries_today()
+            max_queries_per_day = 1000
+
+            if queries_today >= max_queries_per_day:
+                return DatedWord(word_lower=word, first_use=None, was_parsed=False)
+
+            search_result = search_api_for_word_first_use(word)
+            add_word_to_db(search_result.word_lower, search_result.first_use, search_result.was_parsed)
+            return search_result
 
 
-@app.route("/login")
-def login():
-    """Test route login"""
-    return "Test rendering login page"
+def find_word_in_db(word):
+    return DatedWordRecord.query.filter_by(word=word).first()
 
 
+def find_api_queries_today():
+    return len(DatedWordRecord.query.filter_by(lookup_date=date.today()).all())
+
+
+def add_word_to_db(word, first_use_date, first_use_known):
+    dwr = DatedWordRecord(word=word, first_use_date=first_use_date, first_use_known=first_use_known)
+    db.session.add(dwr)
+    db.session.commit()
+
+
+#Model class for dated word database queries
+class DatedWordRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50))
+    first_use_date = db.Column(db.SmallInteger)
+    first_use_known = db.Column(db.Boolean)
+    lookup_date = db.Column(db.Date, default=date.today())
